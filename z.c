42be94772b34e94ebe17f333b4dab1a117ba1f9e
Mishy3ka <Chubachin.misha@bk.ru>
2025-03-31 01:51:27 +0300

    Third message

--- z.c
+++ z.c
@@ -5,8 +5,10 @@ int main(){
 
     sqlite3* db;
     int result = sqlite3_open("test_db", &db);
-    printf("result = %d\n", result);
+    printf("Connecting to the server...\n");
+    if(result == 0) printf("The connection is successful!\n");
     sqlite3_close(db);
+    printf("Disconnecting from the database is successful!\n");
 
     return 0;
 }
